b9b2c39e61acdb9961c0bfabb349fdf16d283375
sunparmar <sunparmar@paypal.com>
2015-03-26 15:13:03 -0700

    updated chart

--- visual/public/javascripts/global.js
+++ visual/public/javascripts/global.js
@@ -11,20 +11,20 @@ $(function () {
             text: 'Source: Real time twitter feed'
         },
         xAxis: {
+            lineColor: 'black',
+            lineWidth: 2,
             crossing:0,
             opposite:true,
-            tickLength : 0,
-            type: 'datetime',
-            dateTimeLabelFormats: { // don't display the dummy year
-                day: '%e. %b',
-                hour: '%b'
-            },
-            title: {
-                text: 'Date'
+            tickLength : 2,
+            labels:
+            {
+              enabled: false
             }
         },
         yAxis: {
+            tickPixelInterval: 15,
             allowDecimals: false,
+            gridLineWidth: 0,
             labels: {
                 formatter: function () {
                     if(this.value == 1)
@@ -37,18 +37,11 @@ $(function () {
                 text: 'Sentiment)'
             }
         },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            floating: true,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-            borderWidth: 2
-        },
+
         plotOptions: {
             scatter: {
                 marker: {
-                    radius: 7,
+                    radius: 11,
                     states: {
                         hover: {
                             enabled: true,
@@ -71,12 +64,12 @@ $(function () {
         },
         series: [{
             name: 'Negative',
-            color: 'rgba(223, 83, 83, 0.7)',
+            color: 'rgba(223, 83, 83, 0.5)',
             data: [[100, -0.25],[100, -0.5],[100, -.75],[100, -1.0],[200, -0.25],[200, -0.50],[200, -0.75]]
 
         }, {
             name: 'Positive',
-            color: 'rgba(80, 180, 80, 0.7)',
+            color: 'rgba(80, 180, 80, 0.5)',
             data: [[100, 0.25],[100, .50],[100, .75],[200,0.25],[200,0.5]]
         }]
     });
